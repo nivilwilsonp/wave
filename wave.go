57e170c7ee61d68f8d5cc6f8603eddf461d57991
Nivil Wilson <nivilwilsonp@gmail.com>
2023-02-07 15:19:38 +0530

    added a new function

--- wave.go
+++ wave.go
@@ -2,6 +2,7 @@ package wave
 
 import (
 	"io"
+	"math"
 	"os"
 	"strings"
 	"time"
@@ -165,4 +166,21 @@ func GetWaveFile() *WaveFile {
 	_wavfile.writeHeader()
 
 	return _wavfile
+}
+
+func WriteSampleData(wav *WaveFile) {
+	var maxAmplitude float64 = 32760
+	var frequency float64 = 250
+	var duration float64 = 2
+
+	var i float64
+	for i = 0; i < float64(wav.Format.SampleRate)*duration; i++ {
+		amplitude := float64(i / float64(wav.Format.SampleRate) * maxAmplitude)
+		value := math.Sin(2 * 3.14 * i * frequency / float64(wav.Format.SampleRate))
+		channel1 := amplitude * value
+		channel2 := (maxAmplitude - amplitude) * value
+
+		wav.WriteData(i16tob(uint16(channel1)))
+		wav.WriteData(i16tob(uint16(channel2)))
+	}
 }
